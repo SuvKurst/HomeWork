605f8c38d5ba05d2b4c4eb5a5442d3503fa9518d
SuvKurst <suvkurst@mail.ru>
2021-04-27 19:33:54 +0300

    Добавьте файлы проекта.

---
+++ lesson1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+//Программа "Анкета"
+
+namespace lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Ваше имя?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Ваша фамилия?");
+            string sName = Console.ReadLine();
+            Console.WriteLine("Ваш возраст?");
+            var age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ваш рост? (В метрах, через запятую)");
+            var tall = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Ваш вес? (В кг через запятую)");
+            var weigh = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(string.Format("{0} {1}, {2} лет, {3} см, {4} кг.", name, sName, age, tall, weigh)); //используя форматированный вывод;
+            Console.WriteLine($"{name} {sName}, {age} лет, {tall} см, {weigh} кг."); //используя вывод со знаком $.
+
+            //Ввести вес и рост человека. Рассчитать и вывести индекс массы тела (ИМТ) по формуле I=m/(h*h); где m — масса тела в килограммах, h — рост в метрах.
+            var I = (weigh / (tall * tall));
+            Console.WriteLine($"Ваш индекс тела составляет - {I}");
+        }
+    }
+
+
+}
