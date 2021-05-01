e7cca377e4f17f83cdd586dff5568a8265f2bac4
SuvKurst <suvkurst@mail.ru>
2021-05-01 21:15:35 +0300

    Выполнены задачи 4,5,6 И 1,2 задача 1го урока

---
+++ lesson1/EX3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EX3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //С клавиатуры вводятся числа, пока не будет введен 0. Подсчитать сумму всех нечетных положительных чисел.
+
+
+
+        }
+    }
+}
