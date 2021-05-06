52682a73632c16720f6ca7a2cf3f81638550c556
SuvKurst <suvkurst@mail.ru>
2021-05-06 16:53:14 +0300

    Выполнены домашние задания по второму и третьему уроку

---
+++ lesson2/EX5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace EX5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //а) Написать программу, которая запрашивает массу и рост человека, вычисляет его индекс массы и сообщает, нужно ли человеку похудеть, набрать вес или всё в норме.
+            //б) *Рассчитать, на сколько кг похудеть или сколько кг набрать для нормализации веса.
+
+            Console.WriteLine("Введите вашу массу в кг:");
+            double weight = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите ваш рост в метрах:");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            double index = weight / (height * 2);
+
+            index = Math.Round(index, 2);
+
+            if (index > 18.5 && index < 25)
+            {
+
+                Console.WriteLine($"Индекс составляет {index}. Ваш вес в норме.");
+            }
+            else if (index < 18.5)
+            {
+                Console.WriteLine($"Индекс составляет {index}. Ваш вес меньше нормы, рекомендуется набрать пару щёчек)");
+            }
+            else
+            {
+                Console.WriteLine($"Индекс составляет {index}. Ваш вес больше нормы, рекомендуется обед разделить с другом, а ужин отдать врагу.");
+            }
+        }
+    }
+}
---
+++ lesson3/EX2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace EX2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //а) С клавиатуры вводятся числа, пока не будет введён 0(каждое число в новой строке).
+            //Требуется подсчитать сумму всех нечётных положительных чисел. Сами числа и сумму вывести на экран, используя tryParse.
+
+
+            int a;
+            int b = 0;
+            do
+            {
+               
+                Console.WriteLine("Введите любые целые числа.");
+                int.TryParse(Console.ReadLine(), out a);
+                 
+
+                if (a % 2 != 0 && a > 0)
+                {
+                    b = b + a; //Складываем нечетные положительные числа в В
+                }
+
+            }
+            while (a != 0);// Повторять пока условие истинно(true)
+
+            Console.WriteLine(Convert.ToString(b));
+
+
+        }
+    }
+}
